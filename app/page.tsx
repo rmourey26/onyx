357fc11378a6a1cb25840a2b0973f71a0c289fbf
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-01-28 17:26:46 -0500

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
@@ -8,6 +9,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <div>
+          <Button />
+        </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
