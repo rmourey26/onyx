28684d8399e324edb2e870ccbde5dcc380e192ed
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-01-28 22:45:44 -0500

    Update config.tsx

---
+++ config/config.tsx
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Onxy",
+  description:
+    "Occams Razor",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    revnet: "https://revnetsystems.com",
+    github: "https://github.com/rmourey26/onyx",
+    aws deployer: "https://github.com/rmourey26/appsync-gpt",
+  },
+}
