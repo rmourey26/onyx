4a399b352da017d22b2080eb9ea02c32d8300067
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-01-28 21:23:28 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div>
-          <Button size="sm" variant="destructive" />
+          <Button size="default" variant="destructive">Destructive</Button>
         </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
