61333b7c6adf9eee2c8f8b3a01eb753429d5569d
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-01-31 22:45:27 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
-    <SiteHeader />
+   <>
+    <SiteHeader>
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
@@ -35,5 +36,7 @@ export default function IndexPage() {
         </Link>
       </div>
     </section>
+    </SiteHeader>
+    </>
   )
 }
