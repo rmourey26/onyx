127857553caa66bff3a67d91a5fbfd97f02f8d9e
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-01 02:52:50 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
 <ThemeProvider
             attribute="class"
             defaultTheme="system"
