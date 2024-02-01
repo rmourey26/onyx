d2c3089eedbb2863f6ff7a89b23347f14da758ba
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-01 03:03:13 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function IndexPage() {
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Onyx <br className="hidden sm:inline" />
-          Embedded systems.
+          embedded systems.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-        Coming soon. 
+         Micro FinTech. 
         </p>
       </div>
       <div className="flex gap-4">
