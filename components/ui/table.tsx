135f0f07363d372285f642cd54e55a9f79cd9f89
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-02 14:44:32 -0500

    Update table.tsx

---
+++ components/ui/table.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react";
+
+export default function Table({
+	children,
+	headers,
+}: {
+	children: ReactNode;
+	headers: string[];
+}) {
+	return (
+		<div className="rounded-md  border-zinc-200 dark:border-zinc-800  w-full overflow-y-auto border  dark:bg-graident-dark">
+			<div className="w-[900px] lg:w-full bg-white dark:bg-inherit rounded-md space-y-5 py-5">
+				<div className=" grid grid-cols-5 px-5 py-2  border-b pb-5 dark:border-zinc-600">
+					{headers.map((header, index) => {
+						return (
+							<h1
+								key={index}
+								className="font-medium text-sm dark:text-gray-500"
+							>
+								{header}
+							</h1>
+						);
+					})}
+				</div>
+
+				{children}
+			</div>
+		</div>
+	);
+}
