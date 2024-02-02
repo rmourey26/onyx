14983c5bc16ca0d526d3e0739256dbe1c8c57fbf
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-02 02:20:23 -0500

    Update page.tsx

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,14 +1,14 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@/utils/supa-server-action'
 import { cookies } from 'next/headers'
 import { Database } from '@/lib/supabase'
 import Todo from './todo'
 
-export default async function ToDos() {
-  const supabase = createServerComponentClient<Database>({ cookies })
+export default async function Page() {
+const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
-
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/')
   return <Todo />
 }
