3e40f3b8b8d6b7a6a0cbc11214f830b5b6f5270f
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-02 13:47:59 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { ReactQueryClientProvider } from '@/components/react-query-client-provider'
-
+import { Toaster } from "@/components/toaster"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -26,7 +26,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            {children}<Toaster/>
           </ThemeProvider>
         
 
