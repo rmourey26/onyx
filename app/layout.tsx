a4d7ac7275f94bc8c8b3b2393da0ec41434c6b1a
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-02 13:48:57 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { ReactQueryClientProvider } from '@/components/react-query-client-provider'
-import { Toaster } from "@/components/toaster"
+import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
