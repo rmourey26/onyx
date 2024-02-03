e57457a4be8b679e6d1419ecd19830a35c116654
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-03 18:19:46 -0500

    Update onboarding-progress.tsx

--- components/onboarding-progress.tsx
+++ components/onboarding-progress.tsx
@@ -8,10 +8,10 @@ export default function OnboardingProgress({ step = 1 }: { step?: number }) {
           <div className="absolute left-0 top-1/2 -mt-px w-full h-0.5 bg-slate-200 dark:bg-slate-700" aria-hidden="true"></div>
           <ul className="relative flex justify-between w-full">
             <li>
-              <Link className={`flex items-center justify-center w-6 h-6 rounded-full text-xs font-semibold ${step >= 1 ? 'bg-indigo-500 text-white' : 'bg-slate-100 dark:bg-slate-700 text-slate-500 dark:text-slate-400'}`} href="/new-program">1</Link>
+              <Link className={`flex items-center justify-center w-6 h-6 rounded-full text-xs font-semibold ${step >= 1 ? 'bg-slate-800 text-white' : 'bg-slate-100 dark:bg-slate-700 text-slate-500 dark:text-slate-400'}`} href="/signup?=page1">1</Link>
             </li>
             <li>
-              <Link className={`flex items-center justify-center w-6 h-6 rounded-full text-xs font-semibold ${step >= 2 ? 'bg-indigo-500 text-white' : 'bg-slate-100 dark:bg-slate-700 text-slate-500 dark:text-slate-400'}`} href="/add-details">2</Link>
+              <Link className={`flex items-center justify-center w-6 h-6 rounded-full text-xs font-semibold ${step >= 2 ? 'bg-slate-800 text-white' : 'bg-slate-100 dark:bg-slate-700 text-slate-500 dark:text-slate-400'}`} href="/add-details">2</Link>
             </li>
             <li>
               <Link className={`flex items-center justify-center w-6 h-6 rounded-full text-xs font-semibold ${step >= 3 ? 'bg-indigo-500 text-white' : 'bg-slate-100 dark:bg-slate-700 text-slate-500 dark:text-slate-400'}`} href="/matching-rules">3</Link>
