b1aad35a12703483c78214c1b56d70f78d981f23
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-03 13:19:21 -0500

    Update Table.tsx

--- components/ui/Table.tsx
+++ components/ui/Table.tsx
@@ -8,7 +8,7 @@ export default function Table({
 	headers: string[];
 }) {
 	return (
-		<div className="rounded-md  border-zinc-200 dark:border-zinc-800  w-full overflow-y-auto border  dark:bg-graident-dark">
+		<div className="rounded-md  border-zinc-200 dark:border-zinc-800  w-full overflow-y-auto border  dark:bg-gradient-dark">
 			<div className="w-[900px] lg:w-full bg-white dark:bg-inherit rounded-md space-y-5 py-5">
 				<div className=" grid grid-cols-5 px-5 py-2  border-b pb-5 dark:border-zinc-600">
 					{headers.map((header, index) => {
