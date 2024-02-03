3ab7ad686b47056be9c9c1ca21d1bc579ece4b5b
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-03 16:54:11 -0500

    Update Page1.tsx

--- components/signup/Page1.tsx
+++ components/signup/Page1.tsx
@@ -8,7 +8,7 @@ import FormikTextField from '@/components/forms/formiktextfield'
 const Page1: FormStepComponentType = (props) => {
   return (
     <div className="flex-col gap-2 w-[400px] box-shadow:0 flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-none transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50">
-      <FormikTextField sx={{ boxShadow:0}} InputProps={{sx:{boxShadow: 0},}} variant="standard" name="email" label="Email" />
+      <FormikTextField className="form-input w-full" sx={{ boxShadow:0}} InputProps={{sx:{boxShadow: 0},}} variant="standard" name="email" label="Email" />
       <FormikTextField sx={{ boxShadow:0}} variant="outlined" name="password" label="Password" type="password" />
       <Button variant="contained" onClick={props.onNext}>
         Next
