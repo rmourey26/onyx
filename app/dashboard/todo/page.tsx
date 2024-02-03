63bcddaf165c7dd7f07568c09dcb37778c1231bb
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-03 03:38:50 -0500

    Update page.tsx

--- app/dashboard/todo/page.tsx
+++ app/dashboard/todo/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import CreateForm from "./components/CreateForm";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import TodoList from "./components/todolist";
+
 export default function Todo() {
 	const todos = [
 		{
