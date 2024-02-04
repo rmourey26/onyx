00be42f46fef3d708ce3ee839ca14f93689815b9
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-04 03:06:16 -0500

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { Icons } from "../icons"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
@@ -42,7 +43,7 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, isLoading = false, disabled:button.Disabled, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, isLoading, disabled:button.Disabled, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     const disabled = isLoading || buttonDisabled
 
