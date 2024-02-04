0369dc7e75858934bbf05abfb3d9cea19fce7ee4
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-03 22:11:26 -0500

    Update SignInForm.tsx

--- app/auth-server-action/components/SignInForm.tsx
+++ app/auth-server-action/components/SignInForm.tsx
@@ -2,7 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import { loginWithEmailAndPassword } from "../actions";
+import { loginWithEmailAndPassword } from "@/app/auth/actions'
 import { AuthTokenResponse } from "@supabase/supabase-js";
 import {
 	Form,
