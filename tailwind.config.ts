890ca57e69353863889e5569d2da905254ca9990
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-05 02:05:58 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,6 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
     },
   },
   plugins: [],
