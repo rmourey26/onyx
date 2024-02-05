c3b37f8a044f812c27b11171999214acdfa07d1f
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-05 20:17:31 +0000

    dash update

--- app/dashboard/components/overview.tsx
+++ app/dashboard/components/overview.tsx
@@ -53,7 +53,7 @@ const data = [
   },
 ]
 
-export default function Overview() {
+export function Overview() {
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart data={data}>
