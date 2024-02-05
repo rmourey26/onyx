06eb64918663235360fa5e3bd306f3231d0725e0
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-05 03:43:13 +0000

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link"
-import { SiteHeader } from "@/components/site-header"
+
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
-   <>
-    <SiteHeader/>
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
@@ -36,6 +34,5 @@ export default function IndexPage() {
         </Link>
       </div>
     </section>
-    </>
   )
 }
