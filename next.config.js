449ff844cf80ef8bcb5d44ac02010a1ca7069be7
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-06 22:00:28 +0000

    remote patterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images : {
-      remotePatterns : ['quantumone.b-cdn.net', 'unpkg.com', 'lh3.googleusercontent.com'],
+      remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'quantumone.b-cdn.net',
+        port: '',
+        pathname: '/onyx/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unpkg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
