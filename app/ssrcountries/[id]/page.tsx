7f803bc664bd679332de7e0794d9be67ba428dca
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-07 22:50:14 -0500

    Update page.tsx

--- app/ssrcountries/[id]/page.tsx
+++ app/ssrcountries/[id]/page.tsx
@@ -3,7 +3,7 @@ import { prefetchQuery } from '@supabase-cache-helpers/postgrest-react-query'
 import useSupabaseServer from '@/utils/supabase-server'
 import { cookies } from 'next/headers'
 import Country from '../country'
-import { getCountryById } from '@/queries/get-country-by-id'
+import { getCountryById } from '@/queries/country-by-id'
 
 export default async function CountryPage({ params }: { params: { id: number } }) {
   const queryClient = new QueryClient()
