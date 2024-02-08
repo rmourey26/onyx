d31041a6f8bce546e77ce278c9928d82f2f08204
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-07 22:36:52 -0500

    Update page.tsx

--- app/countries/[id]/page.tsx
+++ app/countries/[id]/page.tsx
@@ -1,22 +1,28 @@
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
-import { prefetchQuery } from '@supabase-cache-helpers/postgrest-react-query'
-import useSupabaseServer from '@/utils/supabase-server'
-import { cookies } from 'next/headers'
-import Country from '../country'
-import { getCountryById } from '@/queries/get-country-by-id'
+'use client'
 
-export default async function CountryPage({ params }: { params: { id: number } }) {
-  const queryClient = new QueryClient()
-  const cookieStore = cookies()
-  const supabase = useSupabaseServer(cookieStore)
+import useSupabaseBrowser from '@/utils/supabase-browser'
+import { getCountryById } from '@/queries/country-by-id'
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query'
 
-  await prefetchQuery(queryClient, getCountryById(supabase, params.id))
+export default function CountryPage({ params }: { params: { id: number } }) {
+  const supabase = useSupabaseBrowser()
+  const {
+    data: country,
+    isLoading,
+    isError,
+  } = useQuery(getCountryById(supabase, params.id))
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError || !country) {
+    return <div>Error</div>
+  }
 
   return (
-    // Neat! Serialization is now as easy as passing props.
-    // HydrationBoundary is a Client Component, so hydration will happen there.
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Country id={params.id} />
-    </HydrationBoundary>
+    <div>
+      <h1>{country.name}</h1>
+    </div>
   )
 }
