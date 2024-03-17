b33a9f758a4356f47c6fed9337d0b01378853c40
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-03-17 12:37:33 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,25 +39,7 @@ import { publicProvider } from "wagmi/providers/public"
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
-export const viewport: Viewport =  {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-}
+
 interface RootLayoutProps {
   children: React.ReactNode
 }
