91a25ba13536a3f39e57ee9e9c6b0529f342fe14
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-03-20 15:58:40 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import { readUserSession } from "@/utils/actions";
+import { redirect } from "next/navigation";
 
 export default function IndexPage() {
+  const { data: userSession } = await readUserSession();
+
+	if (userSession.session) {
+		return redirect("/dashboard");
+	}
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
