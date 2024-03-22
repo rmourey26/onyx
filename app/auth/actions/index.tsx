82b8997c12376583982e223e550f67d0fbf0448a
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-03-22 15:23:20 -0400

    Update index.tsx

--- app/auth/actions/index.tsx
+++ app/auth/actions/index.tsx
@@ -17,6 +17,20 @@ export async function signInWithGithub() {
   
      const supabase = await createSupbaseServerClient();  
 
+supabase.auth.onAuthStateChange((event, session) => {
+  if (session && session.provider_token) {
+    window.localStorage.setItem('oauth_provider_token', session.provider_token)
+  }
+
+  if (session && session.provider_refresh_token) {
+    window.localStorage.setItem('oauth_provider_refresh_token', session.provider_refresh_token)
+  }
+
+  if (event === 'SIGNED_OUT') {
+    window.localStorage.removeItem('oauth_provider_token')
+    window.localStorage.removeItem('oauth_provider_refresh_token')
+  }
+})
     const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
     options: {
