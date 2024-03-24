1afb5a84a8cee6f08c04fa132f7577708cafaadb
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-03-24 10:35:29 -0400

    Update ui

---
+++ components/ui/wallet-options.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Connector, useConnect } from 'wagmi'
+
+export function WalletOptions() {
+  const { connectors, connect } = useConnect()
+
+  return connectors.map((connector) => (
+    <button key={connector.uid} onClick={() => connect({ connector })}>
+      {connector.name}
+    </button>
+  ))
+}
