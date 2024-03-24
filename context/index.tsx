4c2aa784e89ed73a0a6ec95d205f1ca54a64aaf5
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-03-23 22:01:35 -0400

    Update

---
+++ context/index.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import React, { ReactNode } from 'react'
+import { config, projectId } from '@/config'
+
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { State, WagmiProvider } from 'wagmi'
+
+// Setup queryClient
+const queryClient = new QueryClient()
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+// Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
+
+export default function Web3ModalProvider({
+  children,
+  initialState
+}: {
+  children: ReactNode
+  initialState?: State
+}) {
+  return (
+    <WagmiProvider config={config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
