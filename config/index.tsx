9026eb15a6aacf7631357e7095becb2fe5b31a95
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-03-23 21:59:48 -0400

    Update config

---
+++ config/index.tsx
@@ -0,0 +1,29 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+
+// Get projectId at https://cloud.walletconnect.com
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+const metadata = {
+  name: 'Onyx',
+  description: 'Web3 Connect',
+  url: 'https://onyx-rho-pink.vercel.app', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+  ...wagmiOptions // Optional - Override createConfig parameters
+})
