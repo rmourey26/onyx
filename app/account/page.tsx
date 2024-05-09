27ec1c6662df6e720fbebf5bfadb7b3d8ced00fc
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-09 13:08:36 -0400

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,17 +1,42 @@
+
 import { Separator } from "@/components/ui/separator"
 import AccountForm from "./supa-account-form"
+import { cookies } from 'next/headers'
+import { createClient } from '@/utils/supa-server-actions'
+import { redirect } from 'next/navigation'
+import Link from 'next/link'
 
-export default function SettingsAccountPage() {
+export default async function SettingsAccountPage() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)  
+
+
+  const { 
+data: { user },} = await supabase.auth.getUser()
+  if (!user) {
+    redirect('/auth')
+  }
+
   return (
-    <div className="space-y-6">
-      <div>
-        <h3 className="text-lg font-medium">Account</h3>
-        <p className="text-sm text-muted-foreground">
-          Update your profile..
-        </p>
-      </div>
+    <div className="mt-10 px-2 lg:p-8">
+    <div className="mx-auto px-2 flex w-full flex-col justify-center space-y-6">
+<div className="flex flex-col space-y-2 text-justified-center items-justified-center">
+        <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Account Profile</h1>
+        <p className="text-base text-justify ">
+          Update your account...</p>
+
+        <Link
+          href="/playground"
+          target="_blank"
+          rel="noreferrer"
+          className="text-1xl font-bold tracking-tighter sm:text-3xl"
+        >
+         Check out the OpenAI Playground!
+        </Link>
+       </div>
       <Separator />
-      <AccountForm />
+      <AccountForm user={user}/>
     </div>
+   </div>
   )
 }
