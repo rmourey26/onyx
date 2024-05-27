5cafbaa9cefacd7dd76a62b1c560c0838ed59782
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-27 11:37:29 -0400

    Update components

---
+++ components/site-footer.tsx
@@ -0,0 +1,20 @@
+import { siteConfig } from "@/config/site"
+
+export function SiteFooter() {
+  return (
+    <footer className="py-1 md:px-8 md:py-0 text-center">
+   
+        <p className="text-balance text-center text-xs font-medium leading-loose text-muted-foreground">Copyright © {" "}
+          <a
+            href={siteConfig.links.github}
+            target="_blank"
+            rel="noreferrer"
+            className="text-xs text-center font-medium"
+          >
+            2024 Onyx. 
+          </a>
+        </p>
+    
+    </footer>
+  )
+}
