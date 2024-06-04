3a09ef95bdd137051cf560d9e6e5b17ba560df37
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-04 11:59:34 -0400

    Update components

---
+++ components/auth-button.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { logout } from "@/app/auth/actions";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import React, { useTransition } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { readUserSession } from "@/utils/actions";
+import { redirect } from "next/navigation";
+
+export default async function AuthButton() {
+
+         const { data: userSession } = await readUserSession();
+
+        
+        const [isPending, startTransition] = useTransition();
+        const onSubmit = async () => {
+                startTransition(async () => {
+                        await logout();
+                });
+        };
+
+        
+if (userSession.session) {
+             
+        
+return (
+                <form action={onSubmit}>
+                        <Button
+                                className="w-full flex items-center gap-2"
+                                variant="outline"
+                        >
+                                SignOut{" "}
+                                <AiOutlineLoading3Quarters
+                                        className={cn(" animate-spin", { hidden: !isPending })}
+                                />
+                        </Button>
+                </form>
+        );
+}
+
+else  {
+
+ return (
+
+     <div className="hidden" />
+ 
+ );
+
+}
+
+}
