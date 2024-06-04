03511a27d24b09147d24e3b3ff2e8fbd31506d26
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-04 02:56:05 -0400

    Update config

---
+++ config/docs.ts
@@ -0,0 +1,47 @@
+import { MainNavItem, SidebarNavItem } from "types/nav"
+
+interface DocsConfig {
+  mainNav: MainNavItem[]
+  sidebarNav: SidebarNavItem[]
+}
+
+export const docsConfig: DocsConfig = {
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    { 
+      title: "Account",
+      href: "/account",
+    },
+    { 
+      title: "Dashboard",
+      href: "/dashboard",
+    },
+    { 
+      title: "OpenAI",
+      href: "/playground",
+    },
+    { 
+      title: "RBAC",
+      href: "/members",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Tools",
+      items: [
+        {
+          title: "Sign Out",
+          href: "/signout",
+          items: [],
+        },
+      ],
+    },
+  ],
+}
