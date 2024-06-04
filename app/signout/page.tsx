c62bcacb5c2ddf58aacfa5ec75b36524899b46c2
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-04 03:13:12 -0400

    Update app

---
+++ app/signout/page.tsx
@@ -0,0 +1,19 @@
+import { logout } from "@/app/auth/actions";
+import SignOut from "@/app/dashboard/components/SignOut";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import React, { useTransition } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+export default function page() {
+
+    return (
+          <div className="container px-4 py-8 items-centered">
+          <div className="mt-6">
+                          <h1 className="text-2xl text-center font-semibold tracking-tight mb-4">Sign out of Quantum One </h1>
+            <SignOut/>
+</div>
+</div>
+    )
+        
+}
