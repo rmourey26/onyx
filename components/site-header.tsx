a01823275ca9198d1c7922e59dbfebbe8ccf1c6d
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-04 12:27:12 -0400

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -10,7 +10,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0 xs:space-x0">
         <MainNav items={siteConfig.mainNav} />
     <MobileNav />    
  <div className="flex flex-1 items-center justify-end space-x-4">
