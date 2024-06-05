fb95525809bf9a3402399130496fe3e088a43e78
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-04 23:48:12 -0400

    Update main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,7 +12,7 @@ interface MainNavProps {
 
 export function MainNav({ items }: MainNavProps) {
   return (
-    <div className="flex gap-6 md:gap-10">
+        <div className="mr-2 hidden gap-4 md:flex md:gap-8">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
