ff37f106d25797cea2b4ddd38dd31910894f3e0f
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-18 23:37:51 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require("next-compose-plugins")
+const withMDX = require('@next/mdx')()
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
+
 const nextConfig = {
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  },
+  experimental: {
+    mdxRs: true,
+  },
     images : {
       remotePatterns: [
       {
@@ -20,8 +34,32 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'youtube.com',
+        port: '',
+        pathname: '/embed/HR6a2aHhY_c?si=L2O3Cf7pQ-0HHhsP',
+      },
+
+      {
+        protocol: 'https',
+        hostname: 'quantumone.b-cdn.net',
+        port: '',
+        pathname: '/**',
+      },
+
+      {
+
+        protocol: 'https',
+        hostname: 'api.web3modal.com',
+        port: '',
+      
+      },
     ],
   },
+
+   pageExtensions: ['ts', 'tsx', 'mdx', 'js', 'jsx'],
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
+
