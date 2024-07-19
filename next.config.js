f4f32fe8f877bfca0ec6698bf264826b8f008a86
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-19 18:03:23 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -47,6 +47,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
 
       {
 
