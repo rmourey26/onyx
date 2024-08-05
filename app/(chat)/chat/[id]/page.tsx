01fc41e8473c1226f1968fb545d0f350338ee4d2
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-04 21:52:44 -0400

    Update (chat)

---
+++ app/(chat)/chat/[id]/page.tsx
@@ -0,0 +1,81 @@
+import { type Metadata } from 'next'
+import { cookies } from 'next/headers'
+import { createClient } from '@/utils/supa-server-actions'
+import { nanoid } from '@/lib/utils'
+import { notFound, redirect } from 'next/navigation'
+
+import { auth } from '@/auth'
+import { getChat } from '@/app/actions'
+import { Chat } from '@/components/chat'
+import { AI } from '@/lib/chat/actions'
+import { Session } from '@/lib/types'
+
+export interface ChatPageProps {
+  params: {
+    id: string
+  }
+}
+
+export async function generateMetadata({
+  params
+}: ChatPageProps): Promise<Metadata> {
+  const id = nanoid()
+
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)  
+
+
+  const { 
+data: { user },} = await supabase.auth.getUser()
+  if (!user) {
+    redirect('/auth')
+  }
+
+   
+
+  const chat = await getChat(params.id, user.id)
+  return {
+    title: chat?.title.toString().slice(0, 50) ?? 'Chat'
+  }
+}
+
+export default async function ChatPage({ params }: ChatPageProps) {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)  
+
+
+  const { 
+data: { user },} = await supabase.auth.getUser()
+  if (!user) {
+    redirect(`/auth?next=/chat/${params.id}`)
+  }
+
+  
+  
+
+
+  if (!session?.user) {
+    redirect(`/login?next=/chat/${params.id}`)
+  }
+
+  const userId = user.id as string
+  const chat = await getChat(params.id, userId)
+
+  if (!chat) {
+    redirect('/')
+  }
+
+  if (chat?.userId !== session?.user?.id) {
+    notFound()
+  }
+
+  return (
+    <AI initialAIState={{ chatId: chat.id, messages: chat.messages }}>
+      <Chat
+        id={chat.id}
+        user={user}
+        initialMessages={chat.messages}
+      />
+    </AI>
+  )
+}
