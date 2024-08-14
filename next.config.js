87d838379d9c60229df8f1c9962b88a06f5e2aeb
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-14 20:29:07 +0000

    csp added

--- next.config.js
+++ next.config.js
@@ -6,10 +6,36 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
 });
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    `
+
 const nextConfig = {
   webpack: config => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
   },
   experimental: {
     mdxRs: true,
