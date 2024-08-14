b257a11149de4bf79af30629b48e8c02e596f39f
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-14 01:37:01 -0400

    purgecss postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,28 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  "plugins": [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        "autoprefixer": {
+          "flexbox": "no-2009"
+        },
+        "stage": 3,
+        "features": {
+          "custom-properties": false
+        }
+      }
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+            './app/**/*.{js,jsx,ts,tsx, mdx}',
+            './components/**/*.{js,jsx,ts,tsx,mdx}'
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ["html", "body"]
+      }
+    ],
+  ]
 }
