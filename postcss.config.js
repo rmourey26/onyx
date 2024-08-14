ee9a02b8d8e741e224db3eb032e0f5e4550c44c8
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-14 01:43:19 -0400

    postcss.config.js
    testing purgecss options locally, will add back

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,6 @@
 module.exports = {
-  "plugins": [
-    "postcss-flexbugs-fixes",
-    [
-      "postcss-preset-env",
-      {
-        "autoprefixer": {
-          "flexbox": "no-2009"
-        },
-        "stage": 3,
-        "features": {
-          "custom-properties": false
-        }
-      }
-    ],
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: [
-            './app/**/*.{js,jsx,ts,tsx, mdx}',
-            './components/**/*.{js,jsx,ts,tsx,mdx}'
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ["html", "body"]
-      }
-    ],
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
