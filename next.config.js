12a752af53abe9f3b492488f004d5b2e752951d7
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-20 21:31:55 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,54 @@ const withMDX = require('@next/mdx')()
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
 });
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.supabase.co googleapis.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src *.supabase.co quantumone.b-cdn.net *.unsplash.com youtube.com;
+  connect-src *;
+  font-src 'self' googleapis.com;
+  frame-src *.supabase.co youtube.com
+`
 
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+]
 
 const nextConfig = {
   webpack: config => {
@@ -64,6 +111,15 @@ const nextConfig = {
       },
     ],
   },
+   async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ]
+    },
+
 
    pageExtensions: ['ts', 'tsx', 'mdx', 'js', 'jsx'],
 }
