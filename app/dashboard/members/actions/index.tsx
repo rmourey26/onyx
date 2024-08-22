9582cd34605ff3e846940185cc836ab46b1f24a3
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-22 18:30:01 -0400

    Update index.tsx

--- app/dashboard/members/actions/index.tsx
+++ app/dashboard/members/actions/index.tsx
@@ -1,7 +1,42 @@
-// "use server";
-export async function createMember() {
+"use server";
+
+import { createSupbaseServerClient } from "@/utils/supaone";
+
+import { redirect } from "next/navigation";
+
+type formData = {
+    email: string;
+    password: string;
+    username: string;
+    role: string;
+    status: string; 
+}
+
+export async function createMember(data:formData) {
+const supabase = await createSupbaseServerClient();
+
+try {
+
+const { data: members_table, error } = await supabase
+  .from('members_table')
+  .insert(
+    { email: data.email, password: data.password, username: data.username, role: data.role, status: data.status})
+  .select()
+
+const result = JSON.stringify(data)
+  return result;
+
+
+if (error) throw error
+      alert('Member Created!')
+    } catch (error) {
+      alert('Error updating the data!')
+    }
+}
 	console.log("create user");
 }
+      
+
 export async function updateMemberById(id: string) {
 	console.log("update member");
 }
