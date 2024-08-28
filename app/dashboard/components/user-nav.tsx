6f79b2d94729e4cbcd5b7f4e76916ea892301233
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-27 21:31:22 -0400

    Update user-nav.tsx

--- app/dashboard/components/user-nav.tsx
+++ app/dashboard/components/user-nav.tsx
@@ -21,8 +21,8 @@ import {
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
-              <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-              <AvatarFallback>SC</AvatarFallback>
+              <AvatarImage src="/avatar.png" alt="@onyx" />
+              <AvatarFallback>OX</AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
@@ -31,7 +31,7 @@ import {
             <div className="flex flex-col space-y-1">
               <p className="text-sm font-medium leading-none">shadcn</p>
               <p className="text-xs leading-none text-muted-foreground">
-                m@example.com
+                e@onyx.com
               </p>
             </div>
           </DropdownMenuLabel>
