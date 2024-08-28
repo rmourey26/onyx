0a2cc89d5d23a55fba535c6938f55b69e9b6d211
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-28 01:03:33 -0400

    Update index.tsx

--- app/auth/actions/index.tsx
+++ app/auth/actions/index.tsx
@@ -86,6 +86,35 @@ if (data.url) {
   redirect(data.url)
  }
 }
+export async function signInWithTwitter() {
+
+     const supabase = await createSupbaseServerClient();  
+
+supabase.auth.onAuthStateChange((event, session) => {
+  if (session && session.provider_token) {
+    window.localStorage.setItem('oauth_provider_token', session.provider_token)
+  }
+
+  if (session && session.provider_refresh_token) {
+    window.localStorage.setItem('oauth_provider_refresh_token', session.provider_refresh_token)
+  }
+
+  if (event === 'SIGNED_OUT') {
+    window.localStorage.removeItem('oauth_provider_token')
+    window.localStorage.removeItem('oauth_provider_refresh_token')
+  }
+})
+    const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'twitter',
+    options: {
+       redirectTo: '/auth/callback',
+    },
+  })
+
+if (data.url) {
+  redirect(data.url)
+ }
+}
 export async function logout() {
         const supabase = await createSupbaseServerClient();
         await supabase.auth.signOut();
