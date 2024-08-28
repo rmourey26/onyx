df00f7368e4b857e7ac4d4da4a863c5d3924cc57
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-27 21:31:56 -0400

    Update user-nav.tsx

--- app/dashboard/components/user-nav.tsx
+++ app/dashboard/components/user-nav.tsx
@@ -29,7 +29,7 @@ import {
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">shadcn</p>
+              <p className="text-sm font-medium leading-none">onyx</p>
               <p className="text-xs leading-none text-muted-foreground">
                 e@onyx.com
               </p>
