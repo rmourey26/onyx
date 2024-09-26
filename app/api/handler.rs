95a4db5c6874c08b1f0fd30383d808a796ae66ae
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-09-25 20:51:35 -0400

    Create handler.rs

---
+++ app/api/handler.rs
@@ -0,0 +1,20 @@
+use serde_json::json;
+use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(handler).await
+}
+
+pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(
+            json!({
+              "message": "你好，世界"
+            })
+            .to_string()
+            .into(),
+        )?)
+}
