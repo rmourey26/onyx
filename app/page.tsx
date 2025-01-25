144c19cdadf6b9af26d254f30b2be03e83b5a934
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-01-25 10:09:46 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+uimport Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
@@ -13,12 +13,11 @@ export default async function IndexPage() {
 	}
   return (
         <section className="container mx-auto flex flex-1 flex-col items-center justify-center gap-6 pb-8 pt-2 px-4 py-12 md:px-6 md:py-10 lg:py-24">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Onyx embedded systems.
+      <div className="flex l font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Onyx PWA MVP Template
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-         Micro FinTech. 
+         Secure, scalable, validated CRUD ops, Rust API runtime, and more. 
         </p>
       </div>
       <div className="flex gap-4">
