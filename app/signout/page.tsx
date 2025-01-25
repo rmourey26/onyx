1b4bb9f7288cfcf7aca96ab9998b8fe0e2e40d8b
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-01-25 11:45:10 -0500

    Update page.tsx

--- app/signout/page.tsx
+++ app/signout/page.tsx
@@ -10,7 +10,7 @@ export default function page() {
     return (
           <div className="container px-4 py-8 items-centered">
           <div className="mt-6">
-                          <h1 className="text-2xl text-center font-semibold tracking-tight mb-4">Sign out of Quantum One </h1>
+                          <h1 className="text-2xl text-center font-semibold tracking-tight mb-4">Sign out of Onyx</h1>
             <SignOut/>
 </div>
 </div>
