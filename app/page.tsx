73c8733c20f49f5316954d9b3594e6b7fb35edd9
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-01-25 11:52:38 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,12 @@ export default async function IndexPage() {
 	}
   return (
         <section className="container mx-auto flex flex-1 flex-col items-center justify-center gap-6 pb-8 pt-2 px-4 py-12 md:px-6 md:py-10 lg:py-24">
-      <div className="flex l font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Onyx PWA MVP Template
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Onyx PWA MVP Template.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-         Secure, scalable, validated CRUD ops, Rust API runtime, and more. 
+         Secure, scalable, validated CRUD ops, Rust API runtime, and more.
         </p>
       </div>
       <div className="flex gap-4">
