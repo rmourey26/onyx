383cb3343949dc2b7fdc3e7e7312f3a5848c4335
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-01-25 22:34:57 -0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,7 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://onyx-rho-pink.vercel.app/contact',
       lastModified: new Date(),
-      changeFrequency: 'year',
+      changeFrequency: 'yearly',
       priority: 1,
     },
 
