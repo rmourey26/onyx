4bd3b841caed593441271f31daf4760773fd6f9f
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-01-25 21:40:42 -0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://onyx-rho-pink.vercel.app/',
+      url: 'https://onyx-rho-pink.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
