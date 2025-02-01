87bee754e4a31dd43b309d659814645e6aa23946
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-01 13:20:53 -0500

    Rename page.tsx to page.mdx

--- app/blog/page.tsx
+++
@@ -1,8 +0,0 @@
-# NextMDX blog, docs, embeddings, etc.
-
-## Intuitive ##
-
-- item 1
-- item 2
-- item 3
-- Read more about [Next MDX](https://nextjs.org/docs/pages/building-your-application/configuring/mdx). 
