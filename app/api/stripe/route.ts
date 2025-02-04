fcfe803473dc7813aebdea20c1839ff6302c4f13
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-04 09:02:26 -0500

    Update route.ts

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16", 
+  apiVersion: "2025-01-27.acacia", 
     typescript: true,
     });
 
