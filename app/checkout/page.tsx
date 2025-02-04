0187a680d0f08c841766d3aee288026d44f9dd08
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-04 08:31:06 -0500

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-import CheckoutForm from './CheckoutForm'; // Import your custom form component
+import CheckoutForm from './checkoutForm'; // Import your custom form component
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
