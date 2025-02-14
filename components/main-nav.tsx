edb0172c174a5ea23656464e0f53086d7e243721
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-14 15:15:25 -0500

    Update main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,6 +15,7 @@ export function MainNav({ items }: MainNavProps) {
         <div className="mr-2 hidden gap-4 md:flex md:gap-8">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.resenditIcon className="h-6 w-6" />
+        <span className="inline-block font-bold">{siteConfig.name}</span>
        
       </Link>
       {items?.length ? (
