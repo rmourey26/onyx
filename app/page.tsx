e9c179f7c6314208c4946ba51ac3c942204c569c
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-17 09:14:53 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { readUserSession } from "@/utils/actions";
+import { Features } from "@/components/features"
 import { redirect } from "next/navigation";
 
 export default async function IndexPage() {
@@ -15,12 +16,13 @@ export default async function IndexPage() {
             <section className="container mx-auto flex flex-1 flex-col items-center justify-center gap-6 pb-8 pt-6 px-4 py-12 md:px-6 md:py-10 lg:py-24">
       <div className="flex max-w-[980px] flex-col items-center gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Onyx SaaS PWA Template.
+          Packagings Paradigm Shift 
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-         Validated CRUD ops, user authentication + RBAC, maximum header security, Rust API runtime, TanStack, and more.  
+         Sustainable, brandable, reusable with blockchain and AI technology under the hood - Resend-It. 
         </p>
       </div>
+     <Features/>
       <div className="flex gap-4">
         <Link
           href={siteConfig.links.login}
