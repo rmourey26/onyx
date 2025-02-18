33f6e2fdcd940021cd2fc1c46c737af18aaed95f
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-18 16:40:30 -0500

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        brand:
+          "bg-brand-green-medium text-brand-foreground shadow-sm hover:bg-brand/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
