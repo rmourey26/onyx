89ae3750a0dfe05be4f59e3388cda06171a6d19a
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-19 07:38:12 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,12 @@ import WhyResendItWrapper from "@/components/whyresenditwrapper"
 
 import { redirect } from "next/navigation";
 
+interface YouTubeID {
+  youtubeID : string;
+}
+
+const youtubeID ="UC38fsAFWSN6MP1UZ-8tX_5w"
+
 export default async function IndexPage() {
   const { data: userSession } = await readUserSession();
 
@@ -21,6 +27,9 @@ export default async function IndexPage() {
         <h1 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-4xl mb-8">
           Packagings Paradigm Shift 
         </h1>
+<div className="place-self-center max-w-[980px] aspect-video overflow-hidden rounded-lg shadow-lg ">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/rq0lS7ZKYFQ?si=GTgF6H3Rr78LRA2z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
         <p className="text-lg text-center text-muted-foreground sm:text-xl">
          Sustainable, brandable, reusable with blockchain and AI technology under the hood - Resend-It. 
         </p>
