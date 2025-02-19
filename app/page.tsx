b1f0b041b859f22bf5e0373059776e1254347e98
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-19 07:41:24 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ interface YouTubeID {
   youtubeID : string;
 }
 
-const youtubeID ="UC38fsAFWSN6MP1UZ-8tX_5w"
+const youtubeID ="UC38fsAFWSN6MP1UZ-8tX_5w" as unknown as strong;
 
 export default async function IndexPage() {
   const { data: userSession } = await readUserSession();
