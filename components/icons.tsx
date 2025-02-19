3dba543c258144837ca47bb1f17261007e855bb4
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-18 23:05:47 -0500

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -2,6 +2,7 @@ import {
   LucideProps,
   Moon,
   SunMedium,
+  Menu,
   type Icon as LucideIcon,
 } from "lucide-react"
 
@@ -10,6 +11,7 @@ export type Icon = LucideIcon
 export const Icons = {
   sun: SunMedium,
   moon: Moon,
+  menu: Menu,
   twitter: (props: LucideProps) => (
     <svg
       {...props}
