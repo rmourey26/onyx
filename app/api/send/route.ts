f2d73927bbe77a91983a334d733bd4522ef46e73
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-19 13:33:09 -0500

    Update api

---
+++ app/api/send/route.ts
@@ -0,0 +1,23 @@
+import { EmailTemplate } from "@/components/EmailTemplate";
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
