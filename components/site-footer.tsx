9d1b8a04e68416db37e833c61c7a6a77d5179b56
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-20 13:34:13 -0500

    Update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,20 +1,108 @@
+import Link from "next/link"
 import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
 
 export function SiteFooter() {
   return (
-    <footer className="py-1 md:px-8 md:py-0 text-center">
-   
-        <p className="text-balance text-center text-xs font-medium leading-loose text-muted-foreground">Copyright © {" "}
-          <a
-            href={siteConfig.links.github}
-            target="_blank"
-            rel="noreferrer"
-            className="text-xs text-center font-medium"
-          >
-            2024 Onyx. 
-          </a>
+    <footer className="border-t">
+      <div className="container flex flex-col gap-8 py-8 md:flex-row md:py-12">
+        <div className="flex-1 space-y-4">
+         <Link href="/" target="_blank"
+              rel="noreferrer" className="ml-0">
+          <Icons.logo className="h-6 w-6" />
+          <span className="inline-block font-bold"{siteConfig.name}</span>
+        </Link>
+          <p className="text-sm text-muted-foreground">Pioneering real world asset solutions for the digital age.</p>
+        </div>
+        <div className="grid flex-1 grid-cols-2 gap-12 sm:grid-cols-3">
+          <div className="space-y-4">
+            <h3 className="text-sm font-medium">Solutions</h3>
+            <ul className="space-y-3 text-sm">
+              <li>
+                <Link href="#" className="text-muted-foreground transition-colors hover:text-primary">
+                  Rewards
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact" className="text-muted-foreground transition-colors hover:text-primary">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-4">
+            <h3 className="text-sm font-medium">Company</h3>
+            <ul className="space-y-3 text-sm">
+              <li>
+                <Link href="/about" className="text-muted-foreground transition-colors hover:text-primary">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link href="/blog" className="text-muted-foreground transition-colors hover:text-primary">
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-4">
+            <h3 className="text-sm font-medium">Connect</h3>
+            <div className="flex space-x-4">
+              <Link
+                href={siteConfig.links.github}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  className={buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                  })}
+                >
+                  <Icons.gitHub className="h-5 w-5" />
+                  <span className="sr-only">GitHub</span>
+                </div>
+              </Link>
+              <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.twitter className="h-4 w-4" />
+                <span className="sr-only">Twitter</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.linkedin}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.linkedin className="h-5 w-5" />
+                <span className="sr-only">LinkedIn</span>
+              </div>
+            </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container border-t py-6">
+        <p className="text-center text-sm text-muted-foreground">
+          © {new Date().getFullYear()} Onyx. All Rights Reserved.
         </p>
-    
+      </div>
     </footer>
   )
 }
