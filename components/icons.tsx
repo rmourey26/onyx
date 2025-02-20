2f664209fd6775993e6ef331032181a8ceca61fe
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-20 15:27:29 -0500

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -4,7 +4,7 @@ import {
   SunMedium,
   type Icon as LucideIcon,
   Menu,
-  LinkedIn,
+  Linkedin,
 } from "lucide-react"
 
 export type Icon = LucideIcon
@@ -13,7 +13,7 @@ export const Icons = {
   sun: SunMedium,
   moon: Moon,
   menu: Menu,
-  LinkedIn: LinkedIn,
+  linkedin: Linkedin,
   twitter: (props: LucideProps) => (
     <svg
       {...props}
