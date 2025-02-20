1ffd06f8205f57b47a5fcb65591093e50908ffe2
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-20 18:34:03 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function IndexPage() {
   return (
             <section className="container max-w-7xl mx-auto flex flex-1 flex-col space-y-16 items-center gap-6 pb-32 pt-20 sm:pt-40 sm:pb-24">
       <div className="flex mx-auto flex-col px-4 md:px-6 lg:px-8 items-center text-center gap-2">
-        <h1 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        <h1 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-4xl mb-8">
           Packagings Paradigm Shift 
         </h1>
 
