7d710bcdf3372f921ea8f2b24f23267090745300
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-20 17:04:45 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function IndexPage() {
   return (
             <section className="container max-w-7xl mx-auto flex flex-1 flex-col space-y-16 items-center gap-6 pb-32 pt-20 sm:pt-40 sm:pb-24">
       <div className="flex mx-auto flex-col px-4 md:px-6 lg:px-8 items-center text-center gap-2">
-        <h1 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-4xl mb-8">
+        <h1 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Packagings Paradigm Shift 
         </h1>
 
@@ -32,7 +32,7 @@ export default async function IndexPage() {
 
 <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rq0lS7ZKYFQ?si=GTgF6H3Rr78LRA2z" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 </div>
-        <p className="text-lg text-center text-muted-foreground sm:text-xl">
+        <p className="mt-4 text-lg text-center text-muted-foreground sm:text-xl">
          Sustainable, brandable, reusable with blockchain and AI technology under the hood.  
         </p>
       </div>
