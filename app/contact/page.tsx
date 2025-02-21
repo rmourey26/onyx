9045284426a9caf759df3e2a3cae11643dbfbd9c
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-21 01:33:55 -0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -25,8 +25,8 @@ export default async function ContactPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center items-center">
       <Link href="/" className="flex items-center space-x-2 mb-8">
-        <Icons.resendit className="h-6 w-6" />
-        <span className="inline-block font-bold">{siteConfig.name}</span>
+        <Icons.resendit className="h-6 w-18" />
+      
       </Link>
               <h1 className="text-2xl font-semibold tracking-tight">
                 Contact Us
