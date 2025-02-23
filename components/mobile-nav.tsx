5af4f4341ec375225744d9e676cd8ba7f47cd7c6
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-23 13:02:11 -0500

    Update mobile-nav.tsx

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -24,11 +24,16 @@ export function MobileNav() {
           variant="ghost"
           className="ml-0 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
         >
-                    <Icons.logo className="h-6 w-6 mr-1" />
-<span className="font-bold">{siteConfig.name} </span>
+                    <Icons.menu className="h-6 w-6 mr-1" />
           <span className="sr-only">Toggle Menu</span>
         </Button>
       </SheetTrigger>
+<div className="xs:items-center ml-0 gap-4 md:hidden">
+      <Link href="/" className="flex items-center space-x-2">
+        <Icons.logo className="h-6 w-6" />
+<span className="font-bold">{siteConfig.name}</span>
+      </Link>
+    </div>
       <SheetContent side="left" className="pr-0">
         <MobileLink
           href="/"
