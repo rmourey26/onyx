548ba4a8cf1a2558f7933f9467586bdc2a4606b1
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-03-19 19:51:21 -0400

    Update 3d-cards.tsx

--- components/ui/3d-cards.tsx
+++ components/ui/3d-cards.tsx
@@ -8,7 +8,7 @@ import React, {
   useState,
 } from "react";
 
-import { cn } from "@lib/utils";
+import { cn } from "@/lib/utils";
 
 const MouseEnterContext = createContext<
   [boolean, React.Dispatch<React.SetStateAction<boolean>>] | undefined
