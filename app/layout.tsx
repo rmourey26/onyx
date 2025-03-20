a818dab2b60d6eec015ac05d064dc3afe4081155
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-03-20 03:19:05 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,7 +113,12 @@ export const viewport: Viewport =  {
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 }
+
 interface RootLayoutProps {
   children: React.ReactNode
 }
