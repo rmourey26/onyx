2bf6c035c23eee198fbcc464be449c0e20a6cc6c
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-03-19 20:59:56 -0400

    Update features.tsx

--- components/features.tsx
+++ components/features.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import type React from "react"
-import { BitcoinIcon, Cloud, Shield, Zap, type LucidIcon } from 'lucide-react'
+import { BitcoinIcon, Cloud, Shield, Zap, type LucideIcon } from 'lucide-react'
 import { motion } from "framer-motion"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import Link from "next/link"
