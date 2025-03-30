b5a02e25ac0f6be6df34f8348ddb4db5f2bfa0c9
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-03-29 21:43:08 -0400

    Update supa-server-actions.tsx

--- utils/supa-server-actions.tsx
+++ utils/supa-server-actions.tsx
@@ -1,7 +1,7 @@
 import { type CookieOptions, createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export function createClient(cookieStore: ReturnType<typeof cookies>) {
+export async function createClient(cookieStore: ReturnType<typeof cookies>) {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
