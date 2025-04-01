022bd5f199518baf95eeac276c6e93b030575a15
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-01 08:49:21 -0400

    Update features.tsx

--- components/features.tsx
+++ components/features.tsx
@@ -69,7 +69,7 @@ const Featurez: React.FC = () => {
   return (
     <section className="py-16 bg-background">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center mb-10 text-foreground">Transformative Solutions</h2>
+        <h2 className="text-3xl md:text-4xl lg:text-7xl font-bold text-center mb-10 text-foreground">Transformative Solutions</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {features.map((feature, index) => (
