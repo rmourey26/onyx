05389b5f48a62e5c229b820b8633188e3e730fad
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-18 15:35:06 -0400

    Create actions.ts

---
+++ utils/supabase/actions.ts
@@ -0,0 +1,27 @@
+'use server'; // Ensure this runs only on the server
+
+import { createServerActionClient, type CookieOptions } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { Database } from '@/lib/supabase';
+
+export function createActionClient() {
+  const cookieStore = cookies();
+
+  return createServerActionClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: '', ...options });
+        },
+      },
+    }
+  );
+}
