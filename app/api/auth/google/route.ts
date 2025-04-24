7632649e9be2e5a022650bcb50e0fd62999ddaf5
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-23 20:37:18 -0400

    Create auth/google/route.ts

---
+++ app/api/auth/google/route.ts
@@ -0,0 +1,66 @@
+import
+ { NextResponse } 
+from
+ 
+'next/server'
+;
+import
+ { createClient } 
+from
+ 
+'@/utils/supa-server-actions'
+; 
+
+import { cookies } from 'next/headers'
+
+export
+ 
+async
+ 
+function
+ 
+GET
+(
+) 
+{
+
+const cookieStore = cookies();
+
+const supabase = createClient(cookieStore);
+
+const
+ { user, session, error } = 
+await
+ supabase.auth.signIn({
+    
+provider
+: 
+'google'
+,
+  });
+  
+if
+ (error) {
+    
+return
+ NextResponse.json({ 
+error
+: error.message }, { 
+status
+: 
+401
+ });
+  }
+  
+// Redirect to the callback URL after successful authentication
+
+  
+return
+ NextResponse.redirect(
+new
+ URL(
+'/'
+, process.env.NEXT_PUBLIC_BASE_URL)); 
+// Adjust the redirect URL as necessary
+
+}
