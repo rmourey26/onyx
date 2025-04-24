15aed9e369ebece9de5ea09e95bd378a56cf16d5
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-24 13:39:08 -0400

    Update route.ts

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,32 +1,13 @@
-import
- { NextResponse } 
-from
- 
-'next/server'
-;
-import
- { createClient } from '@/utils/supa-server-actions'
-;
-
+import { NextResponse } from 'next/server';
+import { createClient } from '@/utils/supa-server-actions';
 import { cookies } from 'next/headers';
 
 
 export async function GET(req: Request) {
   
-const
- { searchParams } = 
-new
- URL(req.url);
-  
-const
- accessToken = searchParams.get(
-'access_token'
-);
-  
-const
- refreshToken = searchParams.get(
-'refresh_token'
-);
+ const { searchParams } = newURL(req.url);
+ const accessToken = searchParams.get('access_token');
+ const refreshToken = searchParams.get('refresh_token');
   
 // Get the user from Supabase
 
@@ -34,73 +15,26 @@ const cookieStore = cookies();
 
 const supabase = createClient(cookieStore);
   
-const
- { 
-data
-: { user }, 
-error
-: userError } = 
-await
- supabase.auth.getUser();
+const { data: { user }, error: userError } = await supabase.auth.getUser();
   
-if
- (userError || !user) {
+ if (userError || !user) {
     
-return
- NextResponse.json({ 
-error
-: 
-'User not found'
- }, { 
-status
-: 
-401
- });
+  return NextResponse.json({ error: 'User not found' }, { status: 401});
   }
   
 // Store the refresh token in the user_tokens table
 
-  
-const
- { 
-error
-: tokenError } = 
-await
- supabase
-    .from(
-'user_tokens'
-)
-    .upsert({
-      
-user_id
-: user.id,
-      
-refresh_token
-: refreshToken,
-    });
-  
-if
- (tokenError) {
+const { error: tokenError | null } = await supabase
+    .from('user_tokens')
+    .upsert({ user_id: user.id, refresh_token: refreshToken});
+ 
+  if (tokenError ) {
     
-return
- NextResponse.json({ 
-error
-: tokenError.message }, { 
-status
-: 
-500
- });
+   return NextResponse.json({ error: tokenError.message }, { status: 500});
   }
+ else {
   
-// Redirect to the home page or wherever you want
-
-  
-return
- NextResponse.redirect(
-new
- URL(
-'/'
-, process.env.NEXT_PUBLIC_BASE_URL)); 
+  return NextResponse.redirect(newURL('/', process.env.NEXT_PUBLIC_BASE_URL)); 
 // Adjust the redirect URL as necessary
-
+ }
 }
