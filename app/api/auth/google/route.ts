b539847d1dafcec9a01484d888625ae26f51c99c
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-24 14:35:03 -0400

    Update route.ts

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -19,10 +19,27 @@ const cookieStore = cookies();
 
 const supabase = createClient(cookieStore);
 
+// Register this immediately after calling createClient!
+// Because signInWithOAuth causes a redirect, you need to fetch the
+// provider tokens from the callback.
+
+supabase.auth.onAuthStateChange((event, session) => {
+  if (session && session.provider_token) {
+    window.localStorage.setItem('oauth_provider_token', session.provider_token)
+  }
+  if (session && session.provider_refresh_token) {
+    window.localStorage.setItem('oauth_provider_refresh_token', session.provider_refresh_token)
+  }
+  if (event === 'SIGNED_OUT') {
+    window.localStorage.removeItem('oauth_provider_token')
+    window.localStorage.removeItem('oauth_provider_refresh_token')
+  }
+})
+
 const
  { user, session, error } = 
 await
- supabase.auth.signIn({
+ supabase.auth.signInWithOAuth({
     
 provider
 : 
@@ -47,11 +64,7 @@ status
 
   
 return
- NextResponse.redirect(
-new
- URL(
-'/'
-, process.env.NEXT_PUBLIC_BASE_URL)); 
+ NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_BASE_URL)); 
 // Adjust the redirect URL as necessary
 
 }
