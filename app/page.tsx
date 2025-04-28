9d5647812423e26d77733dd64b8de0ac82bed606
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-28 17:51:25 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,13 @@ export default async function IndexPage() {
         }
   return (
    
-<section className="relative max-w-dvw w-full overflow-hidden">         
-
-<div className="container mx-auto flex flex-col items-center gap-6 pb-32 pt-20 sm:pt-40 sm:pb-24">
-      <div className="flex max-w-7xl flex-col px-4 md:px-6 lg:px-8 justify-center text-center gap-2">
-    
-        <h1 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-4xl mb-8">
+<section className="relative max-w-dvw w-full overflow-hidden pb-16">
+<div className="container px-4 md:px-6 mx-auto flex flex-col space-y-16 items-center pb-16 pt-16 sm:pt-16 sm:pb-24">
+      <div className="flex mx-auto flex-col px-4 md:px-6 lg:px-8 w-full items-center gap-y-12">
+        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-6xl text-center">
           Onyx SaaS PWA Template
         </h1>
-        <p className="max-w-3xl text-lg text-justify text-muted-foreground mb-10 sm:text-xl">
+        <p className="max-w-3xl text-lg lg:text-xl xs:text-justify text-muted-foreground">
          Secure user authentication + RBAC, Zod validated Supabase Postgres DB CRUD ops, Rust serverless API runtime, TanStack queries with Supabase cache helpers, Resend, SID.ai, NextMDX, admin dashboard, and more. Onboard users and receive inquiries immediately. 
         </p>
       </div>
