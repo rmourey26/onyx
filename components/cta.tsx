46876fdc3d1becbcc78b90c5ae0e7f048794a517
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-28 17:53:58 -0400

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -1,14 +1,16 @@
 "use client"
 
+import Link from 'next/link'
+import { siteConfig } from "@/config/site"
 import { motion } from "framer-motion"
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { useState } from 'react' 
+import { useState } from 'react'
 
-export default function CTA() {
+export default function Cta() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 py-4 px-4 sm:py-8 sm:px-8">
+    <section className="py-4 sm:py-6 lg:py-8">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
@@ -23,9 +25,9 @@ export default function CTA() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.2, duration: 0.5 }}
               >
-                <CardTitle className="text-3xl font-extrabold sm:text-4xl">Ready for Onyx?</CardTitle>
-                <CardDescription className="mt-2 text-2xl font-bold text-brand-green-dark dark:text-brand-green-light">
-                  Book a demo today.
+                <CardTitle className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold">Ready for Onyx?</CardTitle>
+                <CardDescription className="mt-2 text-2xl font-bold">
+                  Book a meeting today.
                 </CardDescription>
               </motion.div>
             </CardHeader>
@@ -36,7 +38,7 @@ export default function CTA() {
                 transition={{ delay: 0.3, duration: 0.5 }}
                 className="mt-4 text-lg text-gray-600 dark:text-gray-300"
               >
-                Discover how Onyx is redefining business. 
+                Discover how Onyx is redefining e-commerce. 
               </motion.p>
               <motion.div
                 initial={{ opacity: 0, y: 20 }}
@@ -45,10 +47,15 @@ export default function CTA() {
                 className="mt-8 flex justify-center"
               >
                 <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-                  <Button size="lg"
-            disabled={isLoading} className="px-8 py-3 text-lg font-semibold">
-                    Get started
-                  </Button>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.contact}
+          className={buttonVariants({ variant: "default"})}
+        >
+          Get Started
+        </Link>
+                 
                 </motion.div>
               </motion.div>
             </CardContent>
