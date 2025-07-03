44dff7acf2a366c2e1613a2ba85166bbb9d4e736
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-07-03 12:55:57 -0400

    Update index.tsx

--- app/auth/actions/index.tsx
+++ app/auth/actions/index.tsx
@@ -23,6 +23,24 @@ export async function loginWithEmailAndPassword(data: {
 }) {
         const supabase = await createSupbaseServerClient();
 
+// Register this immediately after calling createClient!
+// Because signInWithOAuth causes a redirect, you need to fetch the
+// provider tokens from the callback.
+supabase.auth.onAuthStateChange((event, session) => {
+  if (session && session.provider_token) {
+    window.localStorage.setItem('oauth_provider_token', session.provider_token)
+  }
+
+  if (session && session.provider_refresh_token) {
+    window.localStorage.setItem('oauth_provider_refresh_token', session.provider_refresh_token)
+  }
+
+  if (event === 'SIGNED_OUT') {
+    window.localStorage.removeItem('oauth_provider_token')
+    window.localStorage.removeItem('oauth_provider_refresh_token')
+  }
+})
+
         const result = await supabase.auth.signInWithPassword(data);
         return JSON.stringify(result);
 }
